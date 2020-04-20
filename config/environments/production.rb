6c43ecaa3e6e4a99802a00ca28172b92eeb1b027
Damien Fleminks <damien@fleminks.com>
2020-04-20 01:20:34 +0100

    Heroku fix 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.serve_static_assets = true
   config.assets.js_compressor = Uglifier.new(:harmony => true)
   config.action_mailer.default_url_options = { host: 'investy-demo.herokuapp.com' }
 end
