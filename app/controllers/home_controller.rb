69ddf8fc92b620f6fd3b242e02e243590c25686d
Damien Fleminks <damien@fleminks.com>
2020-04-20 01:08:40 +0100

    heroky fix 6

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 
 require 'json'
-require  'stockPresenter'
 
 class HomeController < ApplicationController
   before_action :authenticate_user!
@@ -40,4 +39,43 @@ class HomeController < ApplicationController
   # About page
   def about
   end
+end
+
+
+
+class StockPresenter
+
+  def initialize
+      p "initialize:"
+      StockQuote::Stock.new(api_key: 'pk_8dd852fe4adb468dbee87c0459e0b83a')
+  end
+
+
+  #Method to get all stocks
+  def getStocks(userId)
+      stocknames = []
+      Stock.where(:user_id => userId).each{ |item|
+          stocknames.push(item.ticker)
+      }
+      if stocknames.any?
+          return StockQuote::Stock.batch(["quote","logo"],stocknames)
+      else
+          return '{"company_name" : "No Stocks saved", "symbol" : "none"}'
+      end
+  end
+
+
+  #Method to get single stock by symbol
+  def getStock(symbol)
+
+      return StockQuote::Stock.batch(["quote","logo"],symbol)
+
+  end
+
+  # get list of all tickers from the user
+  def auth
+      StockQuote::Stock.new(api_key: 'pk_8dd852fe4adb468dbee87c0459e0b83a')
+  end
+
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Investy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.initialize_on_precompile = false
+    # config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
